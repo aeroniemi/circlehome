b7bcf9b08bbea80fff7856e1782bb221dec39357
Lexi Beavil <alex@aeroniemi.com>
2024-04-01 00:21:45 +0100

    Updated main to use classes

--- src/main.cpp
+++ src/main.cpp
@@ -5,17 +5,29 @@
 #include "sys/sleep_mgmt.h"
 #include <ha_api.h>
 #include "secrets.h"
-#include "ui/screens/screen_loading.h"
-
+// #include "ui/screens/sree.h"
 #include "sys/aero_time.h"
-HomeAssistant * ha;
-void log_cb(lv_log_level_t level, const char * buf)
+HomeAssistant *ha;
+Screen *global_screens[] = {
+    &screen_on_off,
+    // &screen_loading,
+    &screen_entity_menu,
+    &screen_clock};
+Screen_Clock screen_clock;
+Screen_Entity_Menu screen_entity_menu;
+Screen_Loading screen_loading;
+Screen_On_Off screen_on_off;
+void log_cb(lv_log_level_t level, const char *buf)
 {
-  Serial.println(buf);
+    Serial.println(buf);
 }
-
-
-
+void initializeScreens()
+{
+    for (uint8_t i = 0; i < sizeof(global_screens) / sizeof(Screen); i++)
+    {
+        global_screens[i]->initialize();
+    }
+}
 
 void setup()
 {
@@ -24,17 +36,17 @@ void setup()
 
     // lv_log_register_print_cb(log_cb);
     m5dial_lvgl_init();
- 
     M5Dial.Display.setBrightness(70);
-    screen_loading_init();
-    lv_screen_load(screen_loading);
+    screen_loading.initialize();
+    screen_loading.makeActive();
     m5dial_lvgl_next();
     ha = new HomeAssistant(SECRET_WIFI_SSID, SECRET_WIFI_PASSWORD, SECRET_HA_HOSTNAME, SECRET_HA_PORT, SECRET_HA_TOKEN);
     setupTime();
     ha->createEntities();
     ha->updateAllStates();
     ui_init();
-    lv_screen_load(screen_clock);
+
+    // lv_screen_load(screen_clock);
 }
 
 void loop()
