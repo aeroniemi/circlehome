9d39479381295ffe2d864558413f9f4295a23fdb
Lexi Beavil <alex@aeroniemi.com>
2024-03-31 18:13:18 +0100

    Set RTC time on startup

---
+++ src/sys/aero_time.h
@@ -0,0 +1,19 @@
+#include <esp_sntp.h>
+#include <WiFi.h>
+#include <M5Dial.h>
+#include "../secrets.h"
+
+static void sync_time_cb(timeval *tv)
+{
+    time_t t = time(nullptr) + 1; // Advance one second.
+    while (t > time(nullptr))
+        ; // Wait until time has advanced by at least one second
+    M5Dial.Rtc.setDateTime(gmtime(&t));
+}
+inline void setupTime()
+{
+    sntp_set_sync_mode(SNTP_SYNC_MODE_IMMED);
+    sntp_set_time_sync_notification_cb(sync_time_cb);
+    sntp_set_sync_interval(12 * 60 * 60 * 1000UL);
+    configTzTime(NTP_TIMEZONE, NTP_SERVER1, NTP_SERVER2, NTP_SERVER3);
+}
