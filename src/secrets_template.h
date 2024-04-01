92d76b3173d73e60d69697d34f56571479d469a4
Lexi Beavil <alex@aeroniemi.com>
2024-04-01 13:47:00 +0100

    Update secrets_template.h

--- src/secrets_template.h
+++ src/secrets_template.h
@@ -3,3 +3,7 @@
 #define SECRET_HA_HOSTNAME  "homeassistant.local"
 #define SECRET_HA_PORT  8123
 #define SECRET_HA_TOKEN "Bearer [TOKEN]"
+#define NTP_TIMEZONE "Europe/London"
+#define NTP_SERVER1 "0.pool.ntp.org"
+#define NTP_SERVER2 "1.pool.ntp.org"
+#define NTP_SERVER3 "2.pool.ntp.org"
