ac32063b5a747b34125c1bd3f98b3b8fead42108
Lexi Beavil <alex@aeroniemi.com>
2024-04-03 00:38:36 +0100

    Created a font for the clock which is monospaced

--- src/ui/screens/Screen_Clock.h
+++ src/ui/screens/Screen_Clock.h
@@ -14,7 +14,7 @@ class Screen_Clock : public Screen
         clock_text = lv_label_create(_lv_screen);
         lv_obj_center(clock_text);
         lv_label_set_text(clock_text, "18:22:00");
-        lv_obj_set_style_text_font(clock_text, &lv_font_montserrat_48, LV_PART_MAIN | LV_STATE_DEFAULT);
+        lv_obj_set_style_text_font(clock_text, &noto_clock, LV_PART_MAIN | LV_STATE_DEFAULT);
         lv_obj_set_style_text_color(clock_text, lv_color_white(), LV_PART_MAIN | LV_STATE_DEFAULT);
         addTimer(update_time, 1000);
     }
