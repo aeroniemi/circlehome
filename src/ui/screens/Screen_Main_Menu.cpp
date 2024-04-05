e03aeda41c50ef90e92f89d52faa30f9127dde1e
Lexi Beavil <alex@aeroniemi.com>
2024-04-05 15:54:15 +0100

    Define a main menu item struct

--- src/ui/screens/Screen_Main_Menu.cpp
+++ src/ui/screens/Screen_Main_Menu.cpp
@@ -1,5 +1,11 @@
 #include "Screen_Main_Menu.h"
 #include "screens.h"
+Menu_Option main_menu_items[] = {
+    {"Settings", NULL, &screen_main_menu},
+    {"Clock", NULL, &screen_clock},
+    {"Devices", NULL, &screen_entity_menu},
+    {"Loading", NULL, &screen_loading},
+};
 
 void Screen_Main_Menu::create()
 {
--- src/ui/screens/Screen_Main_Menu.h
+++ src/ui/screens/Screen_Main_Menu.h
@@ -3,6 +3,13 @@
 #define SCREEN_MAIN_MENU
 #include <lvgl.h>
 #include "Screen.h"
+#include <Arduino.h>
+struct Menu_Option {
+    String text;
+    const void * image_src;
+    Screen * screen;
+    // Menu_Option(String text, void *image_src, Screen *screen) : text(text), image_src(image_src), screen(screen);
+};
 
 class Screen_Main_Menu : public Screen
 {
