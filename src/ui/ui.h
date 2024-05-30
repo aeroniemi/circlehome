9ce34291bad9e4e9c58037eff45336e6e301ed12
Lexi Beavil <alex@aeroniemi.com>
2024-05-30 23:37:22 +0100

    Update ui.h

--- src/ui/ui.h
+++ src/ui/ui.h
@@ -3,7 +3,9 @@
 #include "screens/screens.h"
 
 #ifndef VERSION
-#define VERSION         STR_VALUE(BUILD_VERSION)
+#define STRINGIZER(arg) #arg
+#define STR_VALUE(arg) STRINGIZER(arg)
+#define VERSION STR_VALUE(BUILD_VERSION)
 #endif
 void ui_init();
 extern void globalEventHandler(lv_event_t *e);
