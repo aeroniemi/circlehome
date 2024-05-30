a2ef378fee4f10b91c5c48474324f78a410b39bb
Lexi Beavil <alex@aeroniemi.com>
2024-05-30 23:29:57 +0100

    Update ui.h

--- src/ui/ui.h
+++ src/ui/ui.h
@@ -1,5 +1,10 @@
 #pragma once
 #include <lvgl.h>
 #include "screens/screens.h"
+
+#ifndef VERSION
+#define VERSION         STR_VALUE(BUILD_VERSION)
+#endif
 void ui_init();
 extern void globalEventHandler(lv_event_t *e);
+
